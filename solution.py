bd42e837cdb87f2e031777ab9c91593338c085a3
Juan <juanmlope@gmail.com>
2024-08-04 16:47:44 -0300

    Add comments in the code

--- solution.py
+++ solution.py
@@ -7,14 +7,17 @@ from datetime import date
 
 class CalcSelic:
     def __init__(self):
+        # Define the absolute path of the current directory
         self._PATH = os.path.abspath(os.getcwd())
 
     def is_valid_input(self, start_date: date, end_date: date) -> list:
-
+        # Check if the dates are date objects 
         if isinstance(start_date, (date)) and isinstance(end_date, (date)):
+            # Check if the start_date is less than the end_date
             if start_date >= end_date:
                 raise Exception('start_date cannot be greater than end_date')
 
+            # Convert the dates to the format DD/MM/YYYY
             start_date = start_date.strftime('%d/%m/%Y')
             end_date = end_date.strftime('%d/%m/%Y')
             return [start_date, end_date]
@@ -24,14 +27,17 @@ class CalcSelic:
         return
 
     def reshape_df(self, df: pd.DataFrame, frequency: str) -> pd.DataFrame:
+        # Set the column 'data' as the index of the DataFrame
         df.set_index('data', inplace=True)
 
+        # Group data by month or year depending on the frequency
         if frequency == 'month':
             df = df.groupby([df.index.year, df.index.month]).tail(1)
         elif frequency == 'year':
             df = df.groupby([df.index.year]).tail(1)
 
         df = df.sort_index()
+        # Calculate the amount earned and rename the column compound
         df["Amount earned"] = df["compound"] - self.capital
         df.drop(['valor'], axis='columns', inplace=True)
         df.rename(columns={'compound': 'Capital'}, inplace=True)
@@ -39,13 +45,19 @@ class CalcSelic:
 
         return df
 
+        # Filter the DataFrame by the specified date range and calculate the total value
     def calc_sum(self, start_date, end_date, df):
+        # Filter the DataFrame to include only the rows within the specified data range
         _df = df[(df['data'] >= start_date) & (df['data'] <= end_date)].copy()
+        # Create a new column 'x' with the inicial capital value
         _df.loc[:, 'x'] = self.capital
+        # Calculate the compounded value over time
         _df['x'] = self.capital * _df['valor'].shift().add(1).cumprod().fillna(1)
+        # Get the final compounded value from the last row in the filtered DataFrame
         val = _df.iloc[-1]['x']
         return val
 
+        # Find the 500-day period with the highest return
     def max_val_range(self, df, range_of=500):
         length = len(df.index) - range_of + 1
         best_start = None
@@ -54,13 +66,16 @@ class CalcSelic:
 
         for i in range(0, length):
             start = df.iloc[i]['data']
+            # DateOffset is used to create a data offset of 'range_of' days
             end = start + pd.DateOffset(days=range_of)
             value = self.calc_sum(start, end, df)
 
+            # Update the best range if the current value is greater
             if value > best_value:
                 best_start = start
                 best_end = end
                 best_value = value
+                # Create a DataFrame for the best range
                 best_df = df[(df['data'] >= best_start) & (df['data'] <= best_end)]
 
         print(
@@ -72,6 +87,7 @@ class CalcSelic:
         return best_df
 
     def save_csv(self, df, file_name):
+        # Check if the CSV file already exists and save the DataFrame if it does not
         files_present = glob.glob(file_name)
         if not files_present:
             df.to_csv(file_name)
@@ -88,11 +104,14 @@ class CalcSelic:
             save_csv: bool,
     ) -> pd.DataFrame:
         self.capital = capital
+        # Validade and format start and end dates
         start_date, end_date = self.is_valid_input(start_date, end_date)
+        # Define the URL to fetch data from the API based on the date range
         base_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json&"
         date_range_str = f'dataInicial={start_date}&dataFinal={end_date}'
         url = base_url + date_range_str
 
+        # Make a request to fetch data from the API
         try:
             resp = requests.get(url)
             df = pd.read_json(resp.text)
@@ -100,6 +119,7 @@ class CalcSelic:
             print(f"Error fetching data from API: {e}")
             return pd.DataFrame()
 
+        # Process API data
         df['data'] = pd.to_datetime(df['data'], format="%d/%m/%Y")
         df['valor'] = pd.to_numeric(df['valor'])
         df.drop_duplicates(subset='data', inplace=True)
@@ -107,12 +127,15 @@ class CalcSelic:
         if df.iloc[0]["data"] < pd.to_datetime(start_date):
             df.drop(index=df.index[0], axis=0, inplace=True)
         df['valor'] = df['valor'] / 100
+        # Create a copy of the raw data
         df_raw = df.copy()
         df['compound'] = capital * (1 + df['valor'].shift()).cumprod().fillna(1)
 
         best_df = self.max_val_range(df)
+        # Find the best period and format the DataFrame according to the frequency
         sol_df = self.reshape_df(best_df, frequency)
 
+        # Save DataFrames to CSV files if needed
         if save_csv:
             self.save_csv(sol_df, file_name=f'solution_{frequency}.csv')
             self.save_csv(df_raw, file_name='df_raw.csv')
--- tests_solution.py
+++ tests_solution.py
@@ -8,20 +8,20 @@ class TestCalcSelic(unittest.TestCase):
         self.calc = CalcSelic()
 
     def test_is_valid_input(self):
-        # Caso válido
+        # Valid case
         start_date = date(2022, 1, 1)
         end_date = date(2023, 1, 1)
         result = self.calc.is_valid_input(start_date, end_date)
         self.assertEqual(result, ['01/01/2022', '01/01/2023'])
 
-        # Caso de erro: start_date > end_date
+        # Error case: start_date > end_date
         start_date = date(2023, 1, 1)
         end_date = date(2022, 1, 1)
         with self.assertRaises(Exception) as context:
             self.calc.is_valid_input(start_date, end_date)
         self.assertTrue('start_date cannot be greater than end_date' in str(context.exception))
 
-        # Caso de erro: inputs não são objetos do tipo date
+        # Error case: inputs are not date objects
         start_date = "2022-01-01"
         end_date = "2023-01-01"
         with self.assertRaises(Exception) as context:
@@ -29,7 +29,7 @@ class TestCalcSelic(unittest.TestCase):
         self.assertTrue('Inputs are in wrong format, shoud be date object' in str(context.exception))
 
     def test_reshape_df_invalid_frequency(self):
-        # Testando para uma frequência inválida
+        # Testing with a invalid frequency
         with self.assertRaises(Exception) as context:
             self.calc.reshape_df(self.df.copy(), 'invalid_frequency')
         self.assertTrue('Invalid frequency' in str(context.exception))
